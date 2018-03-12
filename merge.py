d9fe4ccbf8ee7f8b3ac4921d19556bec61e1bc32
jimbo1qaz <jimbo1qaz@gmail.com>
2018-03-11 22:52:06 -0700

    meh

--- merge.py
+++ merge.py
@@ -1,6 +1,8 @@
 from typing import List
 import itertools
 import numpy as np
+from collections import namedtuple, OrderedDict
+from global_util import *
 
 from . import fourier
 from . import gauss
@@ -9,32 +11,33 @@ assert set_range
 
 
 
-def _I(s, *args, **kwargs):
-    return np.array([int(x, *args, **kwargs) for x in s.split()])
-
-
 def _rms(ar):
     square = np.array(ar) ** 2
     mean = np.mean(square)
     root = np.sqrt(mean)
     return root
 
-
-def load_waves(filename):
-    with open(filename) as f:
-        data = f.read().split(';')
+def load_string(s):
+    data = s.split(';')
     waves = []
     for d in data:
-        wave = _I(d)
+        wave = I(d)
         if wave.size > 0:
             waves.append(wave)
     return np.array(waves)
+
+
+def _load_waves(filename):
+    with open(filename) as f:
+        s = f.read()
+    return load_string(s)
 
 
 # **** merge
 
 
 def _merge_with(*waves: List[np.ndarray], nsamp, avg_func):
+    print(waves)
     ffts = [np.fft.rfft(wave) for wave in waves]
     
     outs = []
@@ -43,7 +46,7 @@ def _merge_with(*waves: List[np.ndarray], nsamp, avg_func):
         arg = np.mean(np.angle(coeffs))
         outs.append(mag * np.exp(1j * arg))
     
-    wave_out = fourier.irfft(outs)
+    wave_out = fourier.irfft(outs, nsamp)
     return gauss.rescale_quantize(wave_out)
 
 
@@ -56,3 +59,89 @@ def power_merge(*waves: List[np.ndarray], nsamp=None):
     return _merge_with(*waves, nsamp=nsamp, avg_func=_rms)
 
 
+# **** packing MML strings
+
+def load_file_mml(filename, mml):
+    waves = _load_waves(filename)
+    inds = I(mml)
+    return waves[inds]
+
+
+Instr = namedtuple('Instr', 'waveseq freq amp')
+# Partial = namedtuple('Instr', 'wave freq amp')
+
+def waveseq_get(waveseq, i):
+    if i >= len(waveseq):
+        return waveseq[-1]
+    return waveseq[i]
+
+
+def _merge_harmonic(instrs: List[Instr], wave_num, nsamp):
+    harmonic_waves = []
+
+    # entry[i] = [freq, amp]
+    for instr in instrs:
+        wave = waveseq_get(instr.waveseq, wave_num)
+        harmonic_wave = npcat([wave] * instr.freq) * instr.amp
+        harmonic_waves.append(harmonic_wave)
+
+    out = power_merge(*harmonic_waves, nsamp=nsamp)
+    return out
+
+
+def _pad_waves(waves, length):
+    return cat(waves, [waves[-1]] * (length - len(waves)))
+
+
+def merge_all(instrs: List[Instr], nsamp):
+    """ Pads each Instr to longest. Then merges all and returns new $waves. """
+    length = max(len(instr.waveseq) for instr in instrs)
+
+    # # [instr#][time] = wave
+    # padded_waveseqs = []
+    # for instr in instrs:
+    #     # [time] time<length
+    #     padded = _pad_waves(instr.waveseq, length)
+    #     padded_waveseqs.append(padded)
+
+    merged_waveseq = []
+
+    # # at time, [instr#].
+    # for simul_waves in safezip(*padded_waveseqs):
+    #     # # list comprehensions?
+    #     # partials = []
+    #     # for instr, wave in safezip(instrs, simul_waves):
+    #     #     partial = Partial(wave, instr.freq, instr.amp)
+    #     #     partials.append(partial)
+
+    #     new_wave = _merge_harmonic(partials)
+    #     merged_waveseq.append(new_wave)
+    
+    for i in range(length):
+        merged_waveseq.append(_merge_harmonic(instrs, i, nsamp))
+
+    return merged_waveseq
+
+def combine(waveseq):
+    """ Returns minimal waveseq, MML string. """
+    waveseq = [tuple(wave) for wave in waveseq]
+    wave2idx = OrderedDict()
+    curr_idx = 0
+
+    mml = []
+
+    for wave in waveseq:
+        if wave not in wave2idx:
+            wave2idx[wave] = curr_idx
+            curr_idx += 1
+        mml.append(wave2idx[wave])
+
+    minimal_waveseq = [S(wave) for wave in wave2idx.keys()]
+    print(';\n'.join(minimal_waveseq))
+    print()
+    print(S(mml))
+
+
+def merge_combine(instrs: List[Instr], nsamp):
+    """ merge and combine into minimal wave and MML string. """
+    combine(merge(instrs, nsamp))
