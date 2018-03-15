535409b861b61325029763b2c286fc696d8bc07d
nyanpasu64 <nyanpasu64@tuta.io>
2018-03-14 22:05:03 -0700

    refactor merge

--- test_merge.py
+++ test_merge.py
@@ -1,4 +1,3 @@
-from pytest import raises
 from wavetable.merge import *
 import numpy as np
 
@@ -9,27 +8,33 @@ def test_merge_waves_mml():
     vol_curve = '0 1 2 3 4 3 2 1 0'
     assert (merge_waves_mml(waves, mml) ==
             np.array([[0, 1, 2, 3],
-                   [0, 1, 3, 6],
-                   [0, 1, 2, 3],
-                   [0, 1, 3, 6]])
+                      [0, 1, 3, 6],
+                      [0, 1, 2, 3],
+                      [0, 1, 3, 6]])
             ).all()
 
     assert (merge_waves_mml(waves, mml, vol_curve) ==
-        np.array([[ 0.,  0.,  0.,  0.],
-               [ 0.,  1.,  3.,  6.],
-               [ 0.,  2.,  4.,  6.],
-               [ 0.,  3.,  9., 18.],
-               [ 0.,  4., 12., 24.],
-               [ 0.,  3.,  9., 18.],
-               [ 0.,  2.,  6., 12.],
-               [ 0.,  1.,  3.,  6.],
-               [ 0.,  0.,  0.,  0.]])
+            np.array([[0., 0., 0., 0.],
+                      [0., 1., 3., 6.],
+                      [0., 2., 4., 6.],
+                      [0., 3., 9., 18.],
+                      [0., 4., 12., 24.],
+                      [0., 3., 9., 18.],
+                      [0., 2., 6., 12.],
+                      [0., 1., 3., 6.],
+                      [0., 0., 0., 0.]])
             ).all()
 
     short_vol = '4 2 1'
     assert (merge_waves_mml(waves, mml, short_vol) ==
-        np.array([[ 0.,  4.,  8., 12.],
-               [ 0.,  2.,  6., 12.],
-               [ 0.,  1.,  2.,  3.],
-               [ 0.,  1.,  3.,  6.]])
+            np.array([[0., 4., 8., 12.],
+                      [0., 2., 6., 12.],
+                      [0., 1., 2., 3.],
+                      [0., 1., 3., 6.]])
             ).all
+
+
+if __name__ == '__main__':
+    import py
+
+    py.test.cmdline.main(sys.argv)
