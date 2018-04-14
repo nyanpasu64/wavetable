9984ba5e8c7bcdc05e46dc8bd8f70b8409cfa5c5
nyanpasu64 <nyanpasu64@tuta.io>
2018-04-14 05:46:54 -0700

    Use MML loop/release points in instrument.

--- instrument.py
+++ instrument.py
@@ -1,14 +1,103 @@
 from numbers import Number
-from typing import List, Sequence, Union
+from typing import List
 
 import numpy as np
-
 from wavetable.wave_util import AttrDict, freq2pitch
+
+HEX = ['0x', '$']
+LOOP = '|'
+RELEASE = '/'
+
+
+class MML(np.ndarray):
+    """
+    >>> m = MML('0 1 2 3 4 | 5 5 5')
+    >>> print(m)
+    0 1 2 3 4 | 5 5 5
+    >>> print(m[1::2])
+    1 3 | 5 5
+    """
+
+    def __new__(cls, string):
+        arr = []
+        loop = None
+        release = None
+
+        a = string.split()
+        for word in a:
+            word0 = word
+            if word == LOOP:
+                loop = len(arr)
+                continue
+            elif word == RELEASE:
+                release = len(arr)
+                continue
+
+            base = 10
+            for hex in HEX:
+                if word.startswith(hex):
+                    base = 16
+                    word = word[len(hex):]
+
+            try:
+                arr.append(int(word, base))
+            except ValueError:
+                print('warning invalid word', word0)
+
+        new = np.asarray(arr).view(cls)
+        new.loop = loop
+        new.release = release
+
+        return new
+
+    def __array_finalize__(self, old):
+        if old is None:
+            return
+        self.loop = getattr(old, 'loop', None)
+        self.release = getattr(old, 'release', None)
+
+    def __str__(self):
+        out = []
+        for i, el in enumerate(self):
+            if i == self.loop:
+                out.append(LOOP)
+            if i == self.release:
+                out.append(RELEASE)
+            out.append(str(el))
+        return ' '.join(out)
+
+    def __repr__(self):
+        return f"{type(self).__name__}('{str(self)}')"
+
+    def __getitem__(self, key):
+        new = np.ndarray.__getitem__(self, key).view(type(self))
+        if isinstance(key, slice):
+            start = key.start or 0
+            step = key.step or 1
+
+            def munge(idx):
+                if idx is None:
+                    return None
+                return (idx - start - 1) // step + 1
+
+            new.loop = munge(new.loop)
+            new.release = munge(new.release)
+        return new
+
+    def __truediv__(self, other):
+        return round(np.ndarray.__truediv__(self, other))
+
+    def __round__(self):
+        # if ndigits is not None:
+        out = np.round(self)
+        return out.astype(int)
 
 
 def S(a, sep=' '):
-    return sep.join(str(x) for x in a)
-
+    if isinstance(a, MML):
+        return str(a)
+    else:
+        return sep.join(str(x) for x in a)
 
 
 class MergeInstr:  # (_Instr):
@@ -35,8 +124,8 @@ class Instr:
 
         for seq_key in self.seqs:
             seq = getattr(self, seq_key)
-            if isinstance(seq, list):
-                setattr(self, seq_key, np.array(seq))
+            if seq is not None:
+                setattr(self, seq_key, np.array(seq).view(MML))
 
     def print_waves(self):
         strs = [S(wave) for wave in self.waveseq]
@@ -66,12 +155,15 @@ class Instr:
 
     def __getitem__(self, get):
         inds = []
-        loop_pos = -1
+        loop_pos = None
+        release_pos = None
 
         for i, elem in enumerate(get):
             if isinstance(elem, str):
-                if elem == '|':
+                if elem == LOOP:
                     loop_pos = len(inds)
+                elif elem == RELEASE:
+                    release_pos = len(inds)
                 else:
                     raise ValueError('cannot get item "%s"' % elem)
             else:
@@ -83,15 +175,16 @@ class Instr:
         for seq_key in self.seqs:
             seq = getattr(self, seq_key)
             if seq is not None:
-                seq = seq[inds]
+                seq = seq[inds]  # .view(MML) # type: MML
+                seq.loop = loop_pos
+                seq.release = release_pos
             setattr(sub_instr, seq_key, seq)
 
         return sub_instr
 
 
 def deduplicate(instr: Instr):
-    return instr    # todo
-
+    return instr  # todo
 
 # def _get(seq, i):
 #     """
