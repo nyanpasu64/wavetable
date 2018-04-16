c9b584989ed145306b20bde3cf457cf6d0f38a0f
nyanpasu64 <nyanpasu64@tuta.io>
2018-04-16 00:01:51 -0700

    Added ZOH-energy-preserving Fourier transforms.

--- fourier.py
+++ fourier.py
@@ -1,31 +1,101 @@
 from typing import List, Union
 
 import numpy as np
+from gauss import nyquist_exclusive, nyquist_inclusive
 
-def irfft(spectrum: Union[np.ndarray, List[complex]], nout=None):
-    """
-    Calculate the inverse Fourier transform of $spectrum, optionally
-    truncating to $nout entries.
 
-    if nout is None, calculate nout = 2*(len-1).
-        nin = (nout//2) + 1.
+# I don't know why, but "inclusive" makes my test cases work.
+NYQUIST = nyquist_inclusive
 
-    if nout is even, realify the last coeff to preserve Nyquist energy.
-    """
-    nout_orig = nout
 
-    if nout is None:
-        nout = 2 * (len(spectrum) - 1)
-    nin = nout // 2 + 1
+def _zoh_transfer(nsamp):
+    nyquist = NYQUIST(nsamp)
+    return np.sinc(np.arange(nyquist) / nsamp)
 
-    if nout_orig is None:
+
+def _realify(a: np.ndarray):
+    return np.copysign(abs(a), a.real)
+
+
+def rfft(signal, *args, **kwargs) -> np.ndarray:
+    """ Computes "normalized" FFT of signal. """
+    return np.fft.rfft(signal, *args, **kwargs) / len(signal)
+
+
+def irfft(spectrum, nsamp=None, *args, **kwargs) -> np.ndarray:
+    """ Computes "normalized" signal of spectrum. """
+    signal = np.fft.irfft(spectrum, nsamp, *args, **kwargs)
+    return signal * len(signal)
+
+
+def rfft_zoh(signal):
+    """ Computes "normalized" FFT of signal, with simulated ZOH frequency response. """
+    nsamp = len(signal)
+    spectrum = rfft(signal)
+
+    # Muffle everything ~~but Nyquist~~, like real hardware.
+    # Nyquist is already real.
+    nyquist = NYQUIST(nsamp)
+    spectrum[:nyquist] *= _zoh_transfer(nsamp)
+
+    return spectrum
+
+
+def zero_pad(spectrum, harmonic):
+    # https://stackoverflow.com/a/5347492/2683842
+    nyquist = len(spectrum) - 1
+    padded = np.zeros(nyquist * harmonic + 1, dtype=complex)
+    padded[::harmonic] = spectrum
+    return padded
+
+
+def irfft_zoh(spectrum: Union[np.ndarray, List[complex]], nsamp=None):
+    """ Computes "normalized" signal of spectrum, counteracting ZOH frequency response. """
+    compute_nsamp = (nsamp is None)
+
+    if nsamp is None:
+        nsamp = 2 * (len(spectrum) - 1)
+    nin = nsamp // 2 + 1
+
+    if compute_nsamp:
         assert nin == len(spectrum)
 
     spectrum = spectrum[:nin].copy()
-    if nout % 2 == 0:
+
+    # Treble-boost everything ~~but Nyquist~~.
+    # Make Nyquist purely real.
+    nyquist = NYQUIST(nsamp)
+    real = nyquist_exclusive(nsamp)
+
+    spectrum[:nyquist] /= _zoh_transfer(nsamp)
+    spectrum[real:] = _realify(spectrum[real:])
+
+    return irfft(spectrum, nsamp)
+
+
+def _irfft_bad(spectrum: Union[np.ndarray, List[complex]], nsamp=None):
+    """
+    Calculate the inverse Fourier transform of $spectrum, optionally
+    truncating to $nsamp entries.
+
+    if nsamp is None, calculate nsamp = 2*(len-1).
+        nin = (nsamp//2) + 1.
+
+    if nsamp is even, realify the last coeff to preserve Nyquist energy.
+    """
+    compute_nsamp = (nsamp is None)
+
+    if nsamp is None:
+        nsamp = 2 * (len(spectrum) - 1)
+    nin = nsamp // 2 + 1
+
+    if compute_nsamp:
+        assert nin == len(spectrum)
+
+    spectrum = spectrum[:nin].copy()
+
+    if nsamp % 2 == 0:
         last = spectrum[-1]
         spectrum[-1] = np.copysign(abs(last), last.real)
 
-    return np.fft.irfft(spectrum, nout)
-
-
+    return irfft(spectrum, nsamp)
