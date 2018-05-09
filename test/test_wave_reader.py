d59d51a4e86b8da32eacf8f725e4ce35b702585e
jimbo1qaz <jimbo1qaz@gmail.com>
2018-05-08 17:10:40 -0700

    Refactor cfg files and read(), and improve unit tests.
    - Fix unrounded_cfg() to supply default fields.
    - Change n163_cfg() to inherit from unrounded_cfg().
    - Config factories now accept dict and keyword arguments.
    - read() uses default "start" point from cfg.

--- test/test_wave_reader.py
+++ test/test_wave_reader.py
@@ -2,34 +2,34 @@ import py.test
 import pytest
 from wavetable.instrument import Instr
 
-from wavetable.wave_util import AttrDict
-from wavetable.wave_reader import WaveReader
+from wavetable.wave_reader import WaveReader, n163_cfg, unrounded_cfg
 
 assert py.test
 
 PATH = 'test_waves/Sample 19.wav'
-FPS = 60
-MAX_RANGE = 16
 NSAMP = 128
 NWAVE = 30
 
-cfg = AttrDict(
-    range=MAX_RANGE,
-    vol_range=16,
-    nsamp=NSAMP,
-    nwave=NWAVE,
-    fps=60,
-    pitch_estimate=74
-)
+
+@pytest.fixture(scope="module", params=[n163_cfg, unrounded_cfg])
+def cfg(request):
+    """ request.param is a cfg factory, taken from params.
+    "request" is a hardcoded name. """
+    yield request.param(
+        nsamp=NSAMP,
+        nwave=NWAVE,
+        fps=60,
+        pitch_estimate=74
+    )
 
 
-def test_wave_at():
+def test_wave_at(cfg):
     read = WaveReader(PATH, cfg)
     wave, *_ = read.wave_at(0)
     assert not (wave == wave[0]).all()
 
 
-def test_wave_reader():
+def test_wave_reader(cfg):
     read = WaveReader(PATH, cfg)
     instr = read.read()
     assert isinstance(instr, Instr)
@@ -38,7 +38,7 @@ def test_wave_reader():
     assert len(waveseq) == NWAVE
 
 
-def test_read_at():
+def test_read_at(cfg):
     inds = [0, 1, 3, 6, 10]
 
     read = WaveReader(PATH, cfg)
@@ -49,7 +49,7 @@ def test_read_at():
     assert len(waveseq) == len(inds)
 
 
-def test_reader_instr():
+def test_reader_instr(cfg):
     read = WaveReader(PATH, cfg)
     instr = read.read()
     assert isinstance(instr, Instr)
@@ -57,7 +57,7 @@ def test_reader_instr():
     instr.print(2, True)
 
 
-def test_subset():
+def test_subset(cfg):
     read = WaveReader(PATH, cfg)
     instr = read.read()
     sub = instr[:20, 20:10:-1]
---
+++ todo.md
@@ -0,0 +1,2 @@
+~~todo: irfft optionally disable nyquist, it ruins the supersaw~~
+- i don't know what was wrong, I'm not working on supersaw anymore
