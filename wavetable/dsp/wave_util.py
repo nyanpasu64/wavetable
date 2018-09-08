302470b66e49399dda14adf82b0ac2c5af318515
nyanpasu64 <nyanpasu64@tuta.io>
2018-09-08 03:23:02 -0700

    Delete AttrDict class

--- wavetable/dsp/wave_util.py
+++ wavetable/dsp/wave_util.py
@@ -9,15 +9,6 @@ def A(*args):
     return np.array(args)
 
 
-# TODO remove
-class AttrDict(dict):
-    def __init__(self, seq=None, **kwargs):
-        if seq is None:
-            seq = {}
-        super(self.__class__, self).__init__(seq, **kwargs)
-        self.__dict__ = self
-
-
 # Phasor merging
 
 
